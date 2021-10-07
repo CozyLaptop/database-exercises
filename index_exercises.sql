e3bb0dceef46d60bcae194eb37ed209d5982c121
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-07 15:24:07 -0500

    init

--- index_exercises.sql
+++ index_exercises.sql
@@ -48,3 +48,83 @@ CREATE TABLE persons (
                          PRIMARY KEY (id),
                          FOREIGN KEY (album_id) REFERENCES albums (id)
 );
+USE employees;
+SELECT employees.emp_no, CONCAT(first_name, ' ', last_name)
+    AS Full_Name, salary, from_date, to_date
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+Use join_test_db;
+CREATE DATABASE IF NOT EXISTS join_test_db;
+
+
+DESCRIBE employees;
+DESCRIBE join_test_db;
+SHOW TABLES;
+use join_test_db;
+SHOW TABLES;
+describe roles;
+
+DROP DATABASE if exists join_test_db;
+SHOW DATABASES;
+CREATE DATABASE IF NOT EXISTS join_test_db;
+Use join_test_db;
+CREATE TABLE roles (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       PRIMARY KEY (id)
+);
+
+CREATE TABLE if not exists users (
+                                     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                     name VARCHAR(100) NOT NULL,
+                                     email VARCHAR(100) NOT NULL,
+                                     role_id INT UNSIGNED DEFAULT NULL,
+                                     PRIMARY KEY (id),
+                                     FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+SHOW TABLES;
+describe roles;
+SELECT * from roles;
+INSERT INTO users (name, email, role_id) VALUES
+('bob', 'bob@example.com', 1),
+('joe', 'joe@example.com', 2),
+('sally', 'sally@example.com', 3),
+('adam', 'adam@example.com', 3),
+('jane', 'jane@example.com', null),
+('mike', 'mike@example.com', null);
+show tables;
+describe users;
+SELECT * from users;
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+SELECT users.name as user_name, roles.name as role_name
+FROM roles
+         LEFT JOIN users ON users.role_id = roles.id;
+use employees;
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+
+use codeup_test_db;
+CREATE TABLE preferences (
+                             person_id INT UNSIGNED NOT NULL,
+                             album_id INT UNSIGNED NOT NULL,
+                             FOREIGN KEY (person_id) REFERENCES persons(id),
+                             FOREIGN KEY (album_id) REFERENCES  albums(id)
+);
+INSERT INTO preferences (person_id, album_id)
+VALUES (1, 12), (1, 5), (1, 22), (1, 29), (2, 1), (2, 31), (2, 30), (3, 11), (3, 26), (3, 25);
---
+++ join_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name)
+AS Full_Name
+FROM departments d
+JOIN dept_manager dm on d.dept_no = dm.dept_no
+JOIN employees ON dm.emp_no = employees.emp_no
+WHERE to_date >= NOW()
+ORDER BY dept_name;
